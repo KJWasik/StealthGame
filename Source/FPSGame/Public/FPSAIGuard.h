bc26a55048470f8935fb9b7bae030c3bfda88742
LAPTOP-OJE1NJ4K\krzys <krzysztofwasik1988@wp.pl>
2019-03-31 20:10:37 +0100

    Enemy added to the project. Working on enemy's AI.

---
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FPSAIGuard.generated.h"
+
+class UPawnSensingComponent;
+
+UCLASS()
+class FPSGAME_API AFPSAIGuard : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFPSAIGuard();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UPawnSensingComponent* PawnSensingComp;
+
+	UFUNCTION()
+	void OnPawnSeen(APawn* SeenPawn);
+
+	UFUNCTION()
+	void OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
